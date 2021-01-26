f4f4402bc1ba2c8131520559dff75cce7863986c
Trevor London <61710584+TrevorLondon@users.noreply.github.com>
2021-01-26 16:36:15 -0700

    Update Mid_Month_Pull.sql

--- January_21_Challenge/Mid_Month_Pull.sql
+++ January_21_Challenge/Mid_Month_Pull.sql
@@ -1,42 +1,67 @@
 --Ran this mid-month to get Mike counts as of the 25th. Filtered out free users per Mike. Live workouts verified and used logic originally 
 -- provided by Garrett and is also what Jeremiah uses. 
 
-SELECT user_type, count(*)
-FROM (
-SELECT user_id,
-       memb_type,
-       subscription_set_to,
-       payment_set_to,
-       user_type,
-       count(*) as wkout_count
+WITH live_wkout_users AS (
+SELECT user_id, count(*) as wkout_count
 from (
 SELECT ul.*,
-       (u.account__subscription_type || ' ' || u.account__payment_type) as memb_type,
-       account_status.subscription_set_to,
-       account_status.payment_set_to,
-       account_status.user_type,
-       CASE WHEN al.workout_context IN ('scheduledPre', 'scheduledLive') OR al.live_workout_schedule_id IS NOT NULL THEN 1 
-            ELSE 0 
-            END AS live_wkout
+       CASE WHEN al.workout_context IN ('scheduledPre', 'scheduledLive') OR live_workout_schedule_id IS NOT NULL THEN 1 ELSE 0 END AS live_wkout
 FROM unique_logs ul
 JOIN prodmongo.activitylogs al on ul.user_id = al.user_id
         AND ul._id = al._id
-JOIN prodmongo.users u on ul.user_id = u._id
-JOIN (
-       SELECT users_id, subscription_set_to, payment_set_to, user_type
-       FROM (
-                select *, 
-                       ROW_NUMBER() OVER (PARTITION BY users_id ORDER BY start_date DESC) as ord_events
-                from users__account_history 
-            )
-       WHERE ord_events = 1
-     ) account_status
-ON ul.user_id = account_status.users_id
-AND CONVERT_TIMEZONE('AMERICA/DENVER',ul."start")::DATE >= '2021-01-01'
-AND user_type IN ('Paid', 'Trial')
+WHERE ul."start"::DATE >= '2021-01-01'
 )
 where live_wkout = 1
-GROUP BY 1,2,3,4,5
-)
-WHERE wkout_count >= 6
 group by 1
+)
+SELECT CASE WHEN parent_id IS NULL THEN 'primary'
+            WHEN parent_id IS NOT NULL THEN 'secondary'
+       END AS primary_or_sec,
+       user_type,
+       count(*)
+FROM (
+SELECT lwu.*,
+       (u.account__subscription_type || ' ' || u.account__payment_type) as memb_type,
+       uah.user_type,
+       CASE WHEN is_secondary = 1 THEN pcm.parent_id ELSE NULL END AS parent_id,
+       CASE WHEN is_secondary = 1 THEN (pcm.parent_sub || ' ' || pcm.parent_pay) ELSE NULL END AS parent_memb
+FROM live_wkout_users lwu
+JOIN prodmongo.users u on lwu.user_id = u._id
+LEFT JOIN (
+      SELECT users_id as parent_id,
+       u.account__subscription_type as parent_sub,
+       u.account__payment_type as parent_pay,
+       "user" as child_user,
+       map.account__subscription_type as child_sub,
+       map.account__payment_type as child_pay,
+       status as child_status
+    FROM prodmongo.users u
+    JOIN (
+        select u._id,
+        u.account__subscription_type,
+        u.account__payment_type,
+        ucu.*
+    FROM prodmongo.users u
+    JOIN prodmongo.users__co_users ucu on u._id = ucu."user"
+    WHERE u.is_secondary = 1
+        ) map
+    on u._id = map.users_id
+   ) pcm
+ON lwu.user_id = pcm.child_user
+LEFT JOIN (
+      SELECT * 
+      FROM (
+      SELECT users_id,
+             user_type,
+             ROW_NUMBER() OVER (PARTITION BY users_id ORDER BY start_date DESC) as ord_events
+      FROM users__account_history uah
+      )
+      WHERE ord_events = 1
+   ) uah
+on lwu.user_id = uah.users_id
+WHERE wkout_count >= 8
+)
+WHERE (parent_id IS NULl AND memb_type NOT LIKE ('%free%') AND memb_type NOT LIKE ('%none%'))
+OR (parent_id IS NOT NULL AND parent_memb NOT LIKE ('%free%') AND parent_memb NOT LIKE ('%none%'))
+OR (user_type = 'Trial')
+GROUP BY 1,2
