d52cd5cd0f309565b5ea1df595e3481caae8bb83
Trevor London <61710584+TrevorLondon@users.noreply.github.com>
2021-07-26 10:13:13 -0600

    Create Video_Wkout_Usage.sql
    Pulled for verification on a metric for Tyson on Jul 25

---
+++ SNOWFLAKE/AD_HOC/Video_Wkout_Usage.sql
@@ -0,0 +1,25 @@
+select date_trunc('week',activity_date)::DATE as wkout_date,
+       count(distinct unique_video_id)
+from analytics_warehouse.fact_activity_log
+where unique_video_id IS NOT NULL
+and activity_date::DATE > getdate()::DATE - 35
+group by 1
+order by 1
+
+
+select count(distinct unique_video_id) 
+from analytics_warehouse.fact_activity_log
+where unique_video_id IS NOT NULL
+and max(activity_date)::DATE > '2020-01-01'
+
+
+select count(*)
+from (
+select unique_video_id,
+       count(activity_log_id) as usage
+from analytics_warehouse.fact_activity_log
+where unique_video_id IS NOT NULL
+and is_current_version = 1
+group by 1
+)
+where usage >= 20
